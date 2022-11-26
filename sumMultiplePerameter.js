ac44f151bbdd56928ef74c95e5b954dbd569e3ac
SMFaysalHaque <smfaysalhaque007@gmail.com>
2022-11-26 19:30:05 +0600

    reverse string and multiple perameter handling

---
+++ sumMultiplePerameter.js
@@ -0,0 +1,17 @@
+// Summation:
+function sum(num1, num2){
+    let sum = num1 + num2;
+    return sum;
+}
+
+let totalSum = sum(50, 80);
+console.log('Total Sum:', totalSum);
+
+// Multiplication:
+function multi(num1, num2){
+    let multi = num1 * num2;
+    return multi;
+}
+
+let totalMulti = multi(50, 80);
+console.log('Total Multiplication:', totalMulti);
